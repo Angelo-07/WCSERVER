ec9161c0b335770a8fb9ee9caf0da73570f8c73e
Angelo-07 <98503954+Angelo-07@users.noreply.github.com>
2022-03-02 23:19:53 +0800

    Add files via upload

---
+++ firstexpress/server.js
@@ -0,0 +1,20 @@
+//name:
+//App Name:
+//Description:
+
+const { response } = require("Express");
+var express = require("Express");
+
+var app = express();
+
+app.get("/", function(request, response){
+  response.send("Successfully created the second App!");
+});
+
+var server = app.listen(2001, 'localhost', function(){
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log('Server running at http://%s:%s', host, port);
+}
+);

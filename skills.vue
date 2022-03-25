b90f84df3ac4c9e744b74ef4d889aa74c9cd0e92
Angelo-07 <98503954+Angelo-07@users.noreply.github.com>
2022-03-25 23:41:47 +0800

    Add files via upload

---
+++ skills.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="skills">
+        <h2>{{ msg3 }}</h2>
+
+        <ul>
+            
+            <li><h4>HTML</h4></li><br>
+            <li><h4>CSS</h4></li><br>
+            <li><h4>JavaScript</h4></li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'skills',
+    data () {
+        return {
+            msg3: 'This is the Skills Component'
+        }
+    }
+}
+</script>
